f65c358f19e8ad793af026f6792e88a76284fbb9
Aayush Kapoor <xeoneux@icloud.com>
2016-05-08 00:34:19 +0530

    Add greeting view component

---
+++ ViewComponents/Greeting.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNet.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class Greeting : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public Greeting(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+
+            return View("Default", model);
+        }
+    }
+}
