ddc8aec2d0409c5a70169a4b679e4b3baa4f81f9
Aayush Kapoor <xeoneux@icloud.com>
2016-05-17 01:32:34 +0530

    Add logout method

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -48,5 +48,13 @@ namespace OdeToFood.Controllers
 
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
