f6be4b4a14f1409ade5aced1ad9b72778fb0aa57
Aayush Kapoor <xeoneux@icloud.com>
2016-05-17 01:23:48 +0530

    Implement register controller

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,12 +1,51 @@
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Mvc;
+using OdeToFood.Entities;
+using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public ViewResult Register()
         {
+            return View();
+        }
+
+        [HttpPost]
+        async public Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var result = await _userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
             return View();
         }
     }
