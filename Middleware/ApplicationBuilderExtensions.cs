f7896e72124198e7e4cc44a961e7d2206faf4b6b
Aayush Kapoor <xeoneux@icloud.com>
2016-05-17 10:04:30 +0530

    Add static files middleware

---
+++ Middleware/ApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+using System.IO;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.AspNet.StaticFiles;
+using Microsoft.Extensions.PlatformAbstractions;
+
+namespace Microsoft.AspNet.Builder
+{
+    public static class ApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseNodeModules(this IApplicationBuilder app, IApplicationEnvironment env)
+        {
+            var path = Path.Combine(env.ApplicationBasePath, "node_modules");
+            var provider = new PhysicalFileProvider(path);
+
+            var options = new StaticFileOptions();
+            options.RequestPath = "/node_modules";
+            options.FileProvider = provider;
+
+            app.UseStaticFiles(options);
+
+            return app;
+        }
+    }
+}
